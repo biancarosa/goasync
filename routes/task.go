3f65db4ef8239df0f18ebabd600f6413c14ce303
Bianca Rosa <me@biancarosa.com.br>
2018-02-16 11:35:00 -0200

    :hammer: remove toml stuff and executes request, logging with logrus

--- routes/task.go
+++ routes/task.go
@@ -1,20 +1,55 @@
 package routes
 
 import (
-	"github.com/biancarosa/goasync/config"
 	"net/http"
 
 	"github.com/labstack/echo"
+	log "github.com/sirupsen/logrus"
 )
+
+//TaskRequest is the struct that defines a request
+type TaskRequest struct {
+	Method string
+	URL    string
+}
 
 //Task is the route that executes a request to the URL of the task with the name same as a the task URL argument
 func Task(ctx echo.Context) error {
-	config := config.ParseTOML()
 	task := ctx.QueryParam("task")
-	for _, endpoint := range config.Endpoints {
-		if endpoint.Name == task {
-			print(endpoint.Name)
-		}
+	req := new(TaskRequest)
+	log.Debug("Binding task request")
+	if err := ctx.Bind(req); err != nil {
+		return err
 	}
+	log.WithFields(log.Fields{
+		"task": task,
+		"req":  req,
+	}).Debug("Execute task")
+	go func() {
+		client := new(http.Client)
+		req, err := http.NewRequest("GET", "http://example.com", nil)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"task":  task,
+				"req":   req,
+				"error": err.Error(),
+			}).Error("An error happened while creating the request.")
+			return
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"task":  task,
+				"req":   req,
+				"error": err.Error(),
+			}).Error("An error happened while executing the request.")
+			return
+		}
+		log.WithFields(log.Fields{
+			"task":     task,
+			"req":      req,
+			"response": resp.Body,
+		}).Info("Everything went fine with the task.")
+	}()
 	return ctx.String(http.StatusOK, "Your task is being executed.")
 }
--- server.go
+++ server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"os"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/biancarosa/goasync/routes"
 )
@@ -10,13 +13,17 @@ import (
 func main() {
 	e := echo.New()
 
+	//Setup Logrus
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	// Routes
 	e.GET("/", routes.HealthCheck)
-	e.GET("/async/:task", routes.Task)
+	e.POST("/async/:task", routes.Task)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
