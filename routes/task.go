60451707a964868497d404c03063fe8c654e3f7d
Bianca Rosa <me@biancarosa.com.br>
2018-02-17 16:50:38 -0200

    :heavy_plus_sign: generate uuid for the task

--- routes/task.go
+++ routes/task.go
@@ -4,32 +4,35 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	uuid "github.com/satori/go.uuid"
 	log "github.com/sirupsen/logrus"
 )
 
-//TaskRequest is the struct that defines a request
-type TaskRequest struct {
+type model struct {
 	Method string
 	URL    string
 	Name   string
+	UUID   uuid.UUID
 }
 
 //Task is the route that executes a request to the URL of the task with the name same as a the task URL argument
 func Task(ctx echo.Context) error {
-	req := new(TaskRequest)
+	task := new(model)
 	log.Debug("Binding task request")
-	if err := ctx.Bind(req); err != nil {
+	if err := ctx.Bind(task); err != nil {
 		return err
 	}
+	log.Debug("Generating uuid")
+	task.UUID = uuid.Must(uuid.NewV4())
 	log.WithFields(log.Fields{
-		"req": req,
+		"task": task,
 	}).Debug("Execute task")
 	go func() {
 		client := new(http.Client)
-		req, err := http.NewRequest("GET", "http://example.com", nil)
+		req, err := http.NewRequest(task.Method, task.URL, nil)
 		if err != nil {
 			log.WithFields(log.Fields{
-				"req":   req,
+				"task":  task,
 				"error": err.Error(),
 			}).Error("An error happened while creating the request.")
 			return
@@ -37,15 +40,15 @@ func Task(ctx echo.Context) error {
 		resp, err := client.Do(req)
 		if err != nil {
 			log.WithFields(log.Fields{
-				"req":   req,
+				"task":  task,
 				"error": err.Error(),
 			}).Error("An error happened while executing the request.")
 			return
 		}
 		log.WithFields(log.Fields{
-			"req":      req,
+			"task":     task,
 			"response": resp.Body,
 		}).Info("Everything went fine with the task.")
 	}()
-	return ctx.String(http.StatusOK, "Your task is being executed.")
+	return ctx.JSON(http.StatusCreated, task)
 }
