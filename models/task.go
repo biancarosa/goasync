685b162ccdc7a5dd6898b122a6908e4db83acc1d
Bianca Rosa <me@biancarosa.com.br>
2018-04-07 12:58:34 -0300

    :art: comments and deletes code

--- models/task.go
+++ models/task.go
@@ -6,6 +6,7 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
+//Task is the structure that contains all relevant information for a task
 type Task struct {
 	Method string
 	URL    string
@@ -13,17 +14,15 @@ type Task struct {
 	UUID   uuid.UUID
 }
 
+//Create creates a task
 func (task *Task) Create() error {
-	session, err := mgo.Dial("localhost")
+	session, err := mgo.Dial("db:27017")
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
 
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	collection := session.DB("test").C("people")
+	collection := session.DB("async").C("tasks")
 	err = collection.Insert(task)
 	return err
 }
